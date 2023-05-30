5100f0f7d0bad469709abb662dc3013321a6f863
scar055 <48519289+scar055@users.noreply.github.com>
2023-05-30 10:36:13 +0200

    ♻️ about naar accordion verandert

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from "vue-router";
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/accordion">Accordion</RouterLink>
       </nav>
     </div>
   </header>
