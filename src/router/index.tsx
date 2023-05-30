5f35879389be55bc25d14d3bc2eeb4403f565154
scar055 <48519289+scar055@users.noreply.github.com>
2023-05-30 11:30:55 +0200

    ✨ alert page added

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,19 +2,24 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/accordion",
-      name: "accordion",
-      component: () => import("../views/AccordionView.vue"),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: HomeView,
+		},
+		{
+			path: "/accordion",
+			name: "accordion",
+			component: () => import("../views/AccordionView.vue"),
+		},
+		{
+			path: "/alert",
+			name: "alert",
+			component: () => import("../views/AlertView.vue"),
+		},
+	],
 });
 
 export default router;
