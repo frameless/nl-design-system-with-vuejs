5d5213fe6f9d9bc0d881d6cda42f6a6d7b9faae8
scar055 <48519289+scar055@users.noreply.github.com>
2023-05-30 16:24:41 +0200

    ✨ profile page added

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ import { RouterLink, RouterView } from "vue-router";
         <RouterLink to="/windowSafe">WindowSafe</RouterLink>
         <RouterLink to="/windowMaatregel">WindowMaatregel</RouterLink>
         <RouterLink to="/windowMaatregelen">WindowMaatregelen</RouterLink>
+        <RouterLink to="/windowProfile">WindowProfile</RouterLink>
         <RouterLink to="/rvoprofiel">RVO Profiel</RouterLink>
         <RouterLink to="/rvozaakview">RVO Zaak</RouterLink>
         <RouterLink to="/rvomijnzaken">RVO Mijn Zaken</RouterLink>
