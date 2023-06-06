59972a9a9060e62a3257e2f011c01913a585f7e9
Nathan Haseth <nathanh1250@gmail.com>
2023-06-06 14:58:56 +0200

    🎨 Import gefixt

--- src/views/JVControleerGegevensView.vue
+++ src/views/JVControleerGegevensView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Paragraph, PageHeader, Heading } from "@utrecht/component-library-vue";
+import { PageHeader, Heading } from "@utrecht/component-library-vue";
 </script>
 
 <template>
