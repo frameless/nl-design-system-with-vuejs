b4030fcc42c7cbd6fc6f2885924092846c849e79
NathanH1250 <nathan.haseth@student.hu.nl>
2023-06-07 16:09:27 +0200

    🎨 Linting

--- src/views/JVSamenvattingView.vue
+++ src/views/JVSamenvattingView.vue
@@ -7,15 +7,15 @@ import { Heading } from "@utrecht/component-library-vue";
 <template>
   <div class="rvo-demo-page">
     <PageHeader class="rvo-header">
-        <div class="rvo-header__logo-wrapper">
-          <a href="/" class="rvo-header__logo-link"
-            ><img
-              src="../images/logo.svg"
-              alt="Rijksdienst voor Ondernemend Nederland, ga naar de startpagina van RVO"
-              class="rvo-logo-img rvo-header__logo-img"
-          /></a>
-        </div>
-      </PageHeader>
+      <div class="rvo-header__logo-wrapper">
+        <a href="/" class="rvo-header__logo-link"
+          ><img
+            src="../images/logo.svg"
+            alt="Rijksdienst voor Ondernemend Nederland, ga naar de startpagina van RVO"
+            class="rvo-logo-img rvo-header__logo-img"
+        /></a>
+      </div>
+    </PageHeader>
     <div class="rvo-layout-column rvo-layout-gap--2xl">
       <div class="rvo-topnav__background">
         <nav
