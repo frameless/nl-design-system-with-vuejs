9546beed512d7ba144e47625b64018c9faa45e42
nathan1250 <98124629+nathan1250@users.noreply.github.com>
2023-06-12 14:34:01 +0200

    main.tsx edits

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "./assets/main.scss";
+import Logo from "./components/Logo.vue";
 
 import { createApp } from "vue";
 import App from "./App.vue";
@@ -7,5 +8,7 @@ import router from "./router";
 const app = createApp(App);
 
 app.use(router);
+
+app.component(Logo, 'Logo');
 
 app.mount("#app");
