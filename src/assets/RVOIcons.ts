fe19d64ce8abf3ebdc0d2cf2cdfb6ead54656c2b
Nathan Haseth <nathanh1250@gmail.com>
2023-06-19 15:24:28 +0200

    ✨ Icons import

--- src/assets/RVOIcons.ts
+++ src/assets/RVOIcons.ts
@@ -1,1 +1,10 @@
-import activiteitenIcon from "@nl-rvo/assets/icons/activiteiten/afhaalpunt.svg”;
+// import activiteitenIcon from "@nl-rvo/assets/icons/activiteiten/afhaalpunt.svg”;
+
+import svgLoader from 'vite-svg-loader'
+export {default as Afhaalpunt} from "@nl-rvo/assets/icons/activiteiten/afhaalpunt.svg?component";
+export {default as Crisisoverleg} from "@nl-rvo/assets/icons/activiteiten/crisisoverleg.svg?component";
+export {default as Dansen} from "@nl-rvo/assets/icons/activiteiten/dansen.svg?component";
+
+// export default defineConfig({
+//   plugins: [vue(), svgLoader()]
+// })
