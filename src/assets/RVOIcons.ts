8811d2eafbda0c70b29edb846fcf0ae14e6aff0a
Nathan Haseth <nathanh1250@gmail.com>
2023-06-19 15:48:06 +0200

    Imports toegevoegd aan RVOIcons

--- src/assets/RVOIcons.ts
+++ src/assets/RVOIcons.ts
@@ -1,9 +1,29 @@
 // import activiteitenIcon from "@nl-rvo/assets/icons/activiteiten/afhaalpunt.svg”;
 
 import svgLoader from 'vite-svg-loader'
+// activiteiten
 export {default as Afhaalpunt} from "@nl-rvo/assets/icons/activiteiten/afhaalpunt.svg?component";
 export {default as Crisisoverleg} from "@nl-rvo/assets/icons/activiteiten/crisisoverleg.svg?component";
 export {default as Dansen} from "@nl-rvo/assets/icons/activiteiten/dansen.svg?component";
+export {default as DocentVoorKlas} from "@nl-rvo/assets/icons/activiteiten/docent-voor-klas.svg?component";
+export {default as Elleboognies} from "@nl-rvo/assets/icons/activiteiten/elleboognies.svg?component";
+export {default as Fysiotherapeut} from "@nl-rvo/assets/icons/activiteiten/fysiotherapeut.svg?component";
+export {default as Gevangene} from "@nl-rvo/assets/icons/activiteiten/gevangene.svg?component";
+export {default as Hulpverleners} from "@nl-rvo/assets/icons/activiteiten/hulpverleners.svg?component";
+export {default as KindInKinderstoel} from "@nl-rvo/assets/icons/activiteiten/kind-in-kinderstoel.svg?component";
+export {default as LopendePersonenMetKoffer} from "@nl-rvo/assets/icons/activiteiten/lopende-personen-met-koffer.svg?component";
+export {default as ManLopend} from "@nl-rvo/assets/icons/activiteiten/man-lopend.svg?component";
+export {default as ManMetLaptop} from "@nl-rvo/assets/icons/activiteiten/man-met-laptop.svg?component";
+export {default as ManMetPuzzelstuk} from "@nl-rvo/assets/icons/activiteiten/man-met-puzzelstuk.svg?component";
+export {default as Ongeval} from "@nl-rvo/assets/icons/activiteiten/ongeval.svg?component";
+export {default as PersonenGooienAutoOmver} from "@nl-rvo/assets/icons/activiteiten/personen-gooien-auto-omver.svg?component";
+export {default as PersonenInGesprek} from "@nl-rvo/assets/icons/activiteiten/personen-in-gesprek.svg?component";
+export {default as PersoonAanBalie} from "@nl-rvo/assets/icons/activiteiten/persoon-aan-balie.svg?component";
+export {default as PersoonAchterStuur} from "@nl-rvo/assets/icons/activiteiten/persoon-achter-stuur.svg?component";
+export {default as PersoonInStoelBijRaamScherm} from "@nl-rvo/assets/icons/activiteiten/persoon-in-stoel-bij-raam-scherm.svg?component";
+export {default as PersoonLopendMetKoffer} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-met-koffer.svg?component";
+export {default as PersoonLopendMetTassen} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-met-tassen.svg?component";
+export {default as PersoonLopendOpZebrapad} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-op-zebrapad.svg?component";
 
 // export default defineConfig({
 //   plugins: [vue(), svgLoader()]
