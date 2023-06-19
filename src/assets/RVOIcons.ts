156bf33938f4788d9f77482b000310535db84025
Nathan Haseth <nathanh1250@gmail.com>
2023-06-19 16:03:20 +0200

    Imports added

--- src/assets/RVOIcons.ts
+++ src/assets/RVOIcons.ts
@@ -24,6 +24,51 @@ export {default as PersoonInStoelBijRaamScherm} from "@nl-rvo/assets/icons/activ
 export {default as PersoonLopendMetKoffer} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-met-koffer.svg?component";
 export {default as PersoonLopendMetTassen} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-met-tassen.svg?component";
 export {default as PersoonLopendOpZebrapad} from "@nl-rvo/assets/icons/activiteiten/persoon-lopend-op-zebrapad.svg?component";
+export {default as PersoonMetCapuchonOpMetLaptop} from "@nl-rvo/assets/icons/activiteiten/persoon-met-capuchon-op-met-laptop.svg?component";
+export {default as PersoonMetDroneBesturing} from "@nl-rvo/assets/icons/activiteiten/persoon-met-drone-besturing.svg?component";
+export {default as PersoonMetSchepEnBergAarde} from "@nl-rvo/assets/icons/activiteiten/persoon-met-schep-en-berg-aarde.svg?component";
+export {default as PersoonMetVerstelbaarBureau} from "@nl-rvo/assets/icons/activiteiten/persoon-met-verstelbaar-bureau.svg?component";
+export {default as PersoonOnderTafel} from "@nl-rvo/assets/icons/activiteiten/persoon-onder-tafel.svg?component";
+export {default as PersoonOpHoverboard} from "@nl-rvo/assets/icons/activiteiten/persoon-op-hoverboard.svg?component";
+export {default as PersoonOpZonnebank} from "@nl-rvo/assets/icons/activiteiten/persoon-op-zonnebank.svg?component";
+export {default as PersoonZittendInStoel} from "@nl-rvo/assets/icons/activiteiten/persoon-zittend-in-stoel.svg?component";
+export {default as PresentatieVoorGroep} from "@nl-rvo/assets/icons/activiteiten/presentatie-voor-groep.svg?component";
+export {default as RennendKind} from "@nl-rvo/assets/icons/activiteiten/rennend-kind.svg?component";
+export {default as RennendPersoonMetRugzak} from "@nl-rvo/assets/icons/activiteiten/rennend-persoon-met-rugzak.svg?component";
+export {default as Sauna} from "@nl-rvo/assets/icons/activiteiten/sauna.svg?component";
+export {default as SilhouetVoorBeeldscherm} from "@nl-rvo/assets/icons/activiteiten/silhouet-voor-beeldscherm.svg?component";
+export {default as SilhouetVoorSchermRaamMetSilhouet} from "@nl-rvo/assets/icons/activiteiten/silhouet-voor-scherm-raam-met-silhouet.svg?component";
+export {default as SlapendPersoon} from "@nl-rvo/assets/icons/activiteiten/slapend-persoon.svg?component";
+export {default as SnelLopendeVrouw} from "@nl-rvo/assets/icons/activiteiten/snel-lopende-vrouw.svg?component";
+export {default as SpelendKindEnKlimrekOnderDak} from "@nl-rvo/assets/icons/activiteiten/spelend-kind-en-klimrek-onder-dak.svg?component";
+export {default as SprekerVoorPubliek} from "@nl-rvo/assets/icons/activiteiten/spreker-voor-publiek.svg?component";
+export {default as Trouwen} from "@nl-rvo/assets/icons/activiteiten/trouwen.svg?component";
+export {default as VrouwAchterKatheder} from "@nl-rvo/assets/icons/activiteiten/vrouw-achter-katheder.svg?component";
+export {default as VrouwLopend} from "@nl-rvo/assets/icons/activiteiten/vrouw-lopend.svg?component";
+export {default as VrouwMetLaptop} from "@nl-rvo/assets/icons/activiteiten/vrouw-met-laptop.svg?component";
+export {default as VrouwMetPortemonnee} from "@nl-rvo/assets/icons/activiteiten/vrouw-met-portemonnee.svg?component";
+export {default as WachtendPersoon} from "@nl-rvo/assets/icons/activiteiten/wachtend-persoon.svg?component";
+export {default as ZittendPersoonAchterLaptop} from "@nl-rvo/assets/icons/activiteiten/zittend-persoon-achter-laptop.svg?component";
+
+// beeld-en-geluid
+export {default as Beveiligingscamera} from "@nl-rvo/assets/icons/beeld-en-geluid/beveiligingscamera.svg?component";
+export {default as BrandendeLampMetCopyrightTeken} from "@nl-rvo/assets/icons/beeld-en-geluid/brandende-lamp-met-copyright-teken.svg?component";
+export {default as FilmProjector} from "@nl-rvo/assets/icons/beeld-en-geluid/film-projector.svg?component";
+export {default as FilmstripMetPlusteken} from "@nl-rvo/assets/icons/beeld-en-geluid/filmstrip-met-plusteken.svg?component";
+export {default as Fotocamera} from "@nl-rvo/assets/icons/beeld-en-geluid/fotocamera.svg?component";
+export {default as Handmicrofoon} from "@nl-rvo/assets/icons/beeld-en-geluid/handmicrofoon.svg?component";
+export {default as HangendeSpot} from "@nl-rvo/assets/icons/beeld-en-geluid/hangende-spot.svg?component";
+export {default as KoptelefoonOverMicrofoon} from "@nl-rvo/assets/icons/beeld-en-geluid/koptelefoon-over-microfoon.svg?component";
+export {default as Koptelefoon} from "@nl-rvo/assets/icons/beeld-en-geluid/koptelefoon.svg?component";
+export {default as Megafoon} from "@nl-rvo/assets/icons/beeld-en-geluid/megafoon.svg?component";
+export {default as Microfoon} from "@nl-rvo/assets/icons/beeld-en-geluid/microfoon.svg?component";
+export {default as Oordopjes} from "@nl-rvo/assets/icons/beeld-en-geluid/oordopjes.svg?component";
+export {default as StapelFotos} from "@nl-rvo/assets/icons/beeld-en-geluid/stapel-fotos.svg?component";
+export {default as VerbodFotografie} from "@nl-rvo/assets/icons/beeld-en-geluid/verbod-fotografie.svg?component";
+export {default as Videocamera} from "@nl-rvo/assets/icons/beeld-en-geluid/videocamera.svg?component";
+export {default as VirtualReality} from "@nl-rvo/assets/icons/beeld-en-geluid/virtual-reality.svg?component";
+
+
 
 // export default defineConfig({
 //   plugins: [vue(), svgLoader()]
