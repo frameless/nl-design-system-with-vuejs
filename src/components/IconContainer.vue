78ecdc48ae5d75a2f6f3bda0a16a16dac7d0059a
Nathan Haseth <nathanh1250@gmail.com>
2023-06-21 10:28:07 +0200

    pre-refinement

--- src/components/IconContainer.vue
+++ src/components/IconContainer.vue
@@ -1,3 +1,11 @@
-<script setup lang="ts"></script>
-
+<script setup>
+export default {
+  props: SVG,
+};
+</script>
+<div>
+      {{ 'SVG' }}  
+    </div>
 <template></template>
+
+<style scoped></style>
