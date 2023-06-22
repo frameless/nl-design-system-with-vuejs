4290af64b1b8b24bc99d3b027d24445508c68bf8
Nathan Haseth <nathanh1250@gmail.com>
2023-06-22 16:10:55 +0200

    Paragraphs, headers LRKIhelpView

---
+++ src/components/PageFooterLRK.vue
@@ -0,0 +1,172 @@
+<script setup lang="ts">
+import { PageFooter } from "@utrecht/component-library-vue";
+import { UnorderedList } from "@utrecht/component-library-vue";
+import { UnorderedListItem } from "@utrecht/component-library-vue";
+</script>
+
+<template>
+  <PageFooter>
+    <footer class="rvo-footer">
+      <div class="rvo-max-width-layout rvo-max-width-layout--lg">
+        <UnorderedList class="rvo-footer-menu">
+          <UnorderedListItem class="rvo-footer-menu-title">
+            <Heading :level="3"><b>Algemeen</b></Heading>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Help</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Toegankelijkheid</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Webservices</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Privacy</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Sitemap</a>
+          </UnorderedListItem>
+        </UnorderedList>
+        <UnorderedList class="rvo-footer-menu">
+          <UnorderedListItem class="rvo-footer-menu-title">
+            <Heading :level="3"><b>Hoe werkt</b></Heading>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Machtigingen en TAN-codes</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Zaken regelen bij RVO</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Inschrijven en registreren</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Bedrijfsoverdracht melden</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Overlijden melden</a>
+          </UnorderedListItem>
+        </UnorderedList>
+        <UnorderedList class="rvo-footer-menu">
+          <UnorderedListItem class="rvo-footer-menu-title">
+            <Heading :level="3"><b>Service</b></Heading>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Abonneren op nieuwsbrief</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Klantenpanel</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Berichtenbox voor bedrijven</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">RVO.nl</a>
+          </UnorderedListItem>
+        </UnorderedList>
+        <UnorderedList class="rvo-footer-menu">
+          <UnorderedListItem class="rvo-footer-menu-title">
+            <Heading :level="3"><b>Over deze site</b></Heading>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Contactgegevens</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Contactformulier</a>
+          </UnorderedListItem>
+          <UnorderedListItem class="rvo-footer-menu-item">
+            <span
+              class="utrecht-icon rvo-icon rvo-icon-delta-naar-rechts rvo-icon--sm rvo-icon--wit"
+              role="img"
+              aria-label="Delta naar rechts"
+            ></span
+            ><a href="#" class="rvo-link">Aanvraag voorlichting/spreker</a>
+          </UnorderedListItem>
+        </UnorderedList>
+      </div>
+    </footer>
+  </PageFooter>
+</template>
+
+<style scoped></style>
