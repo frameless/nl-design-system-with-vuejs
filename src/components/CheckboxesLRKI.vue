2fdd685bd5158280f8dadc0b7dd2e02c4e4a1686
NathanH1250 <nathan.haseth@student.hu.nl>
2023-06-28 00:11:48 +0200

    ✨ Checkboxes in rows

--- src/components/CheckboxesLRKI.vue
+++ src/components/CheckboxesLRKI.vue
@@ -5,67 +5,108 @@ import { Paragraph } from "@utrecht/component-library-vue";
 </script>
 
 <template>
-  <div>
-    <Heading :level="2">Zoekresultaten</Heading>
-    <br />
-    <strong>Filter</strong>
-    <br />
-    <Paragraph>Gezocht op kinderopvangvoorziening: </Paragraph>
-    <br />
-    <strong>Soort</strong>
-    <br />
-  </div>
-  <div class="rvo-checkbox__group row">
-    <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionA">
-      <input
-        id="optionA"
-        name="group"
-        class="rvo-checkbox__input"
-        type="checkbox"
-        value=""
-      />
-      Kinderdagverblijf (0-4 jaar)
-    </label>
-    <label class="rvo-checkbox rvo-checkbox--checked" for="optionB">
-      <input
-        id="optionB"
-        name="group"
-        class="rvo-checkbox__input"
-        type="checkbox"
-        checked=""
-        value=""
-      />
-      Buitenschoolse opvang (4-13 jaar)
-    </label>
-    <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionC">
-      <input
-        id="optionC"
-        name="group"
-        class="rvo-checkbox__input"
-        type="checkbox"
-        value=""
-      />
-      Gastouderopvang (0-13 jaar)
-    </label>
-    <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionD">
-      <input
-        id="optionD"
-        name="group"
-        class="rvo-checkbox__input"
-        type="checkbox"
-        value=""
-      />
-      Gastouderbureau
-    </label>
-    <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
-      <input
-        id="optionD"
-        name="group"
-        class="rvo-checkbox__input"
-        type="checkbox"
-        value=""
-      />
-      Voormalig peuterspeelzaal
-    </label>
+  <div class="lrki-container">
+    <div class="checkbox-column">
+      <strong>Soort</strong>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionA">
+        <input
+          id="optionA"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Kinderdagverblijf (0-4 jaar)
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--checked" for="optionB">
+        <input
+          id="optionB"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          checked=""
+          value=""
+        />
+        Buitenschoolse opvang (4-13 jaar)
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionC">
+        <input
+          id="optionC"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Gastouderopvang (0-13 jaar)
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionD">
+        <input
+          id="optionD"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Gastouderbureau
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
+        <input
+          id="optionE"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Voormalig peuterspeelzaal
+      </label>
+    </div>
+    <div class="checkbox-row">
+      <div class="checkbox-column">
+        <strong>Extra</strong>
+        <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
+        <input
+          id="optionE"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Voorschoolse educatie
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
+        <input
+          id="optionE"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Ouderparticipatiecrèche
+      </label>
+      </div>
+      <div class="checkbox-column">
+        <strong>Status</strong>
+        <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
+        <input
+          id="optionE"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Geregistreerd
+      </label>
+      <label class="rvo-checkbox rvo-checkbox--not-checked" for="optionE">
+        <input
+          id="optionE"
+          name="group"
+          class="rvo-checkbox__input"
+          type="checkbox"
+          value=""
+        />
+        Niet meer geregistreerd
+      </label>
+      </div>
+    </div>
   </div>
 </template>
